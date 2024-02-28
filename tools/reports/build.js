8a4ec5c044ce6dcbfc39cb37c7ce623135d7327e
kaoru2010 <254566+kaoru2010@users.noreply.github.com>
2024-02-28 12:57:04 +0900

    Added multiple-cucumber-html-reporter.

---
+++ tools/reports/build.js
@@ -0,0 +1,8 @@
+// https://www.npmjs.com/package/multiple-cucumber-html-reporter
+
+require("multiple-cucumber-html-reporter");
+
+report.generate({
+  jsonDir: "./json",
+  reportPath: "./report",
+});
